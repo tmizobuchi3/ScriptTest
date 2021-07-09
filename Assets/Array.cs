10805f2f023e9894a0cedbce47127b3340e7788b
tmizobuchi3 <83824309+tmizobuchi3@users.noreply.github.com>
2021-07-09 18:53:37 +0900

    Initial commit

---
+++ Assets/Array.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    
+
+
+int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = new int[5];
+
+        array[0] = 11;
+        array[1] = 12;
+        array[2] = 13;
+        array[3] = 14;
+        array[4] = 15;
+
+        for (i = 0; i < 5; ++i)
+
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (i = 4; i > -1; --i)
+
+        {
+            Debug.Log(array[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    public int hp;
+    public int power;
+    public int mp;
+
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+    }
+
+
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else if (mp < 5)
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+}
+
+
+
+public class Test : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        Boss midboss = new Boss();
+
+        midboss.hp = 50;
+        midboss.power = 10;
+        midboss.mp = 53;
+
+        midboss.Attack();
+        midboss.Defence(4);
+
+        for (int i = 1; i < 12; i++)
+        {
+            midboss.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
